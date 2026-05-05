7d242bfd67504fe81a3dd9ce34b0bae27d43a011
sleyder02 <ramosleiderdavid@gmail.com>
2026-05-04 22:49:04 -0500

    + archivo modificado seguimiento-comunitarioP

---
+++ seguimiento-comunitario.js
@@ -0,0 +1,15 @@
+let semana = 1
+const SemanaDeSeguimiento = 4
+let propuestasAtendidas = 0
+
+console.log("Seguimiento comunitario")
+
+while (semana <= SemanaDeSeguimiento) {
+  console.log("semana de seguimiento número:", semana)
+  propuestasAtendidas = propuestasAtendidas + 2
+  console.log("Propuestas atendidas hasta ahora:", propuestasAtendidas) 
+  semana = semana + 1 
+}
+
+console.log("seguimiento comunitario finalizado.")
+console.log("Propuestas atendidas:", propuestasAtendidas)
